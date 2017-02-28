a24477e2cf55893a7561d71fad2cedbc687cdd5d
Alex <olefirenk@gmail.com>
2017-02-28 14:12:46 +0200

    +snackbar reducer

---
+++ src/reducers/snackbar.js
@@ -0,0 +1,14 @@
+import Immutable from "immutable"
+
+export default (state = Immutable.Map({}), action) => {
+    switch (action.type) {
+
+        case "@@snackbar/open":
+            return state.set(action.meta, {open: true, message: action.payload})
+
+        case "@@snackbar/close":
+            return state.set(action.meta, {open: false, message: action.payload})
+        default:
+            return state
+    }
+}
