0e6da947a68185f73b0c95125f44cd01bcede4f0
Alex <olefirenk@gmail.com>
2017-02-28 15:04:32 +0200

    ver 1.0.15

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,13 @@
 import React from "react"
 import LoginForm from "../components/Login/LoginForm"
 import {connect} from "react-redux"
-import {bindActionCreators} from "redux"
 import {login} from "../actions/Security"
 import {push} from "react-router-redux"
 
-@connect((state)=>({
-    user: state.authentication.user
-}), (dispatch)=>({
-    actions: bindActionCreators({login, push}, dispatch)
-}))
+@connect(null, {login, push})
 export default class Login extends React.Component {
     render() {
-        const {login, push} = this.props.actions
-
+        const {login, push} = this.props
         return (
             <div>
                 <LoginForm onSubmit={login} onSubmitSuccess={()=>push("/")}/>
