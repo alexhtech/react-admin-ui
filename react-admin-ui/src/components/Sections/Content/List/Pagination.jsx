86642bea1910c06625b56f6413793929b1e68c57
Alex <olefirenk@gmail.com>
2017-08-11 19:12:56 +0300

    fix

--- react-admin-ui/src/components/Sections/Content/List/Pagination.jsx
+++ react-admin-ui/src/components/Sections/Content/List/Pagination.jsx
@@ -49,7 +49,7 @@ export default (Component) => class Pagination extends React.Component {
         const entity = getEntity(this.props.entityName)
 
         //configure pagination settings
-        this.configure(entity.list.pagination)
+        this.configure(entity.actions.list.pagination)
 
         //give back Component if pagination is disabled
         if (this.settings.disable) return <Component {...this.props}/>
